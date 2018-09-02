cc2e226330a948976c12559921c827c72821d817
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-02 23:14:20 +0200

    Reduce bundle size by pruning highlight.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import MiniTutorial from "mini-tutorial.js";
 import mtTheme from "mini-tutorial.js/themes/white.css";
 
-import hljs from "highlight.js";
+import hljs from "highlight.js/lib/highlight";
+import hljsLangXML from "highlight.js/lib/languages/xml";
 import hljsStyle from "highlight.js/styles/atom-one-light.css";
 
 window.addEventListener("load", () => {
     let mt = new MiniTutorial();
     mt.start();
 
+    hljs.registerLanguage('html', hljsLangXML);
     document.querySelectorAll("pre code").forEach(el => hljs.highlightBlock(el));
 });
