bcb752d0eba8c975e9f81dc5666f974822f75c71
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-02 23:06:11 +0200

    Initial content

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import MiniTutorial from "mini-tutorial.js";
-import mtTheme from "mini-tutorial.js/themes/white.css"
+import mtTheme from "mini-tutorial.js/themes/white.css";
+
+import hljs from "highlight.js";
+import hljsStyle from "highlight.js/styles/atom-one-light.css";
 
 window.addEventListener("load", () => {
     let mt = new MiniTutorial();
     mt.start();
+
+    document.querySelectorAll("pre code").forEach(el => hljs.highlightBlock(el));
 });
