3efd0314d53dc1b8c7e83b0eaf30894bb6b0af37
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-02 23:26:58 +0200

    Adding JavaScript syntax highlighting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import mtTheme from "mini-tutorial.js/themes/white.css";
 
 import hljs from "highlight.js/lib/highlight";
 import hljsLangXML from "highlight.js/lib/languages/xml";
+import hljsLangJS from "highlight.js/lib/languages/javascript";
 import hljsStyle from "highlight.js/styles/atom-one-light.css";
 
 window.addEventListener("load", () => {
@@ -10,5 +11,6 @@ window.addEventListener("load", () => {
     mt.start();
 
     hljs.registerLanguage('html', hljsLangXML);
+    hljs.registerLanguage('javascript', hljsLangJS);
     document.querySelectorAll("pre code").forEach(el => hljs.highlightBlock(el));
 });
