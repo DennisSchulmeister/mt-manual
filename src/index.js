0fef79168e8fc9a55077f7314eea66074053ee75
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-26 17:03:44 +0200

    Reworked the manual for mini-tutorial.js 1.0.6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import MiniTutorial from "mini-tutorial.js";
-import mtTheme from "mini-tutorial.js/themes/white.css";
+import mtTheme from "mini-tutorial.js/themes/white.css"; // Must be first
+import mtCommonCSS from "mini-tutorial.js/themes/common.css";
 
 import hljs from "highlight.js/lib/highlight";
 import hljsLangXML from "highlight.js/lib/languages/xml";
+import hljsLangCSS from "highlight.js/lib/languages/css";
 import hljsLangJS from "highlight.js/lib/languages/javascript";
 import hljsStyle from "highlight.js/styles/atom-one-light.css";
 
@@ -11,6 +13,7 @@ window.addEventListener("load", () => {
     mt.start();
 
     hljs.registerLanguage("html", hljsLangXML);
+    hljs.registerLanguage("css", hljsLangCSS);
     hljs.registerLanguage("javascript", hljsLangJS);
     document.querySelectorAll("pre code").forEach(el => hljs.highlightBlock(el));
 });
