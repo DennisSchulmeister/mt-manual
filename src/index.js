264d51c59f235778517951cb1b59363123426f0d
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-27 12:38:55 +0200

    Adopt index.js to new version with async load possibility

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+"use strict";
+
 import MiniTutorial from "mini-tutorial.js";
-import mtTheme from "mini-tutorial.js/themes/white.css"; // Must be first
-import mtCommonCSS from "mini-tutorial.js/themes/common.css";
+import mtThemeWhite from "mini-tutorial.js/themes/white.css";
+import mtThemeCommon from "mini-tutorial.js/themes/common.css";
+import myStylesheet from "./style.css";
 
 import hljs from "highlight.js/lib/highlight";
 import hljsLangXML from "highlight.js/lib/languages/xml";
@@ -8,12 +11,42 @@ import hljsLangCSS from "highlight.js/lib/languages/css";
 import hljsLangJS from "highlight.js/lib/languages/javascript";
 import hljsStyle from "highlight.js/styles/atom-one-light.css";
 
-window.addEventListener("load", () => {
-    let mt = new MiniTutorial();
-    mt.start();
+async function fetchChapters(chapters, callback) {
+    let response = null;
+    let html = "";
+
+    for(let i = 0; i < chapters.length; i++) {
+        response = await fetch(chapters[i]);
+        html += await response.text();
+    }
+
+    if (document.readyState == "complete") {
+        callback(html);
+    } else {
+        window.addEventListener("load", () => callback(html));
+    }
+}
+
+function startApplication(html) {
+    if (html) {
+        let main = document.querySelector("main");
+        main.innerHTML = html;
+    }
 
     hljs.registerLanguage("html", hljsLangXML);
     hljs.registerLanguage("css", hljsLangCSS);
     hljs.registerLanguage("javascript", hljsLangJS);
     document.querySelectorAll("pre code").forEach(el => hljs.highlightBlock(el));
-});
+
+    let mt = new MiniTutorial();
+    mt.start();
+}
+
+fetchChapters([
+    // Add HTML files here if you want to split the document.
+    // The HTML files should only contain <section>-elements without <html>/<head>/<body>
+    //
+    // "01-intro.html",
+    // "02-chapter2.html",
+    // ...
+], startApplication);
